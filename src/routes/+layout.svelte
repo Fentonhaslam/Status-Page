7b9f3b3199d68763cb4d6ce64e8c02fd3a6111da
Fenton Haslam <fentonhaslam@gmail.com>
2025-03-12 14:57:42 +0000

    removed bottom part

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,6 @@
 <footer class="footer footer-center bg-base-300 text-base-content p-4 sticky top-[100vh]">
 	<aside>
 		<p>
-			<a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub-Status-Page</a> made with ❤️
-			by <a href="https://bullrich.dev">Bullrich</a>
 		</p>
 	</aside>
 </footer>
